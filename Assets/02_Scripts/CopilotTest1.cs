575a21e5681ab371677c05748d9afc24a2dc31cc
DESKTOP-K1D1PPL\thirs <wmdash92@gmail.com>
2021-11-03 23:18:05 +0900

    VSCodeCommit

---
+++ Assets/02_Scripts/CopilotTest1.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopilotTest1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //singleton pattern
+    public static CopilotTest1 Instance { get; private set; }
+    //    
+
+
+}
